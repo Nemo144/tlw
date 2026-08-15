fdfbd63d6654a2a7c9f200c33ddfadbd4d00de9d
Nnamdi Okeke <onnamdialex@gmail.com>
2026-08-15 15:45:54 +0100

    :white_check_mark: Add test for the lock function

--- tests/tlw.test.ts
+++ tests/tlw.test.ts
@@ -1,20 +1,108 @@
-import { describe, expect, it } from "vitest";
+import { Cl } from "@stacks/transactions";
+import { describe, expect, it, test } from "vitest";
 
 const accounts = simnet.getAccounts();
 const address1 = accounts.get("wallet_1")!;
 
-/*
-The test below is an example. To learn more, read the testing documentation here:
-https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
+describe("Testing lock", () => {
+  test("Allows the contract owner to lock an amount", () => {
+    const deployer = accounts.get("deployer")!;
+    const beneficiary = accounts.get("wallet_1")!;
+    const amount = 10;
 
-describe("example tests", () => {
-it("ensures simnet is well initialised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-});
+    const lockResponse = simnet.callPublicFn(
+      "timelocked-wallet",
+      "lock",
+      [Cl.principal(beneficiary), Cl.uint(10), Cl.uint(amount)],
+      deployer,
+    );
 
-// it("shows an example", () => {
-//   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-//   expect(result).toBeUint(0);
-// });
+    // The lock should be successful.
+    expect(lockResponse.result).toBeOk(Cl.bool(true));
+
+    // There should be a STX transfer of the amount specified.
+    expect(lockResponse.events).toHaveLength(1);
+    expect(lockResponse.events[0].event).toBe("stx_transfer_event");
+
+    expect(lockResponse.events[0].data).toMatchObject({
+      amount: amount.toString(),
+      sender: deployer,
+      recipient: `${deployer}.timelocked-wallet`,
+    });
+  });
+
+  test("Does not allow anyone else to lock an amount", () => {
+    const accountA = accounts.get("wallet_1")!;
+    const beneficiary = accounts.get("wallet_2")!;
+
+    const lockResponse = simnet.callPublicFn(
+      "timelocked-wallet",
+      "lock",
+      [Cl.principal(beneficiary), Cl.uint(10), Cl.uint(10)],
+      accountA,
+    );
+
+    // Should return err-owner-only (err u100).
+    expect(lockResponse.result).toBeErr(Cl.uint(100));
+  });
+
+  test("Cannot lock more than once", () => {
+    const deployer = accounts.get("deployer")!;
+    const beneficiary = accounts.get("wallet_1")!;
+    const unlockAt = 10;
+    const amount = 10;
+
+    const lockResponse1 = simnet.callPublicFn(
+      "timelocked-wallet",
+      "lock",
+      [Cl.principal(beneficiary), Cl.uint(unlockAt), Cl.uint(amount)],
+      deployer,
+    );
+    const lockResponse2 = simnet.callPublicFn(
+      "timelocked-wallet",
+      "lock",
+      [Cl.principal(beneficiary), Cl.uint(unlockAt), Cl.uint(amount)],
+      deployer,
+    );
+
+    // The first lock worked and STX were transferred.
+    expect(lockResponse1.result).toBeOk(Cl.bool(true));
+    expect(lockResponse1.events).toHaveLength(1);
+    expect(lockResponse1.events[0].event).toBe("stx_transfer_event");
+
+    expect(lockResponse1.events[0].data).toMatchObject({
+      amount: amount.toString(),
+      sender: deployer,
+      recipient: `${deployer}.timelocked-wallet`,
+    });
+
+    // The second lock fails with err-already-locked (err u101).
+    expect(lockResponse2.result).toBeErr(Cl.uint(101));
+
+    // Assert there are no transfer events.
+    expect(lockResponse2.events).toHaveLength(0);
+  });
+
+  test("Unlock height cannot be in the past", () => {
+    const deployer = accounts.get("deployer")!;
+    const beneficiary = accounts.get("wallet_1")!;
+    const amount = 10;
+    const targetBlockHeight = 10;
+
+    // Advance the chain until the unlock height plus one.
+    simnet.mineEmptyBlocks(targetBlockHeight + 1);
+
+    const lockResponse = simnet.callPublicFn(
+      "timelocked-wallet",
+      "lock",
+      [Cl.principal(beneficiary), Cl.uint(targetBlockHeight), Cl.uint(amount)],
+      deployer,
+    );
+
+    // The lock fails with err-unlock-in-past (err u102).
+    expect(lockResponse.result).toBeErr(Cl.uint(102));
+
+    // Assert there are no transfer events.
+    expect(lockResponse.events).toHaveLength(0);
+  });
 });
